acc5350d2aee60870d2face943fa60f382e312f4
Intigam Mammadli <intigam.mammadli@gmail.com>
2023-02-18 13:56:45 +0400

    serializers and api views created

--- backend/quiz/views.py
+++ backend/quiz/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, status, parsers
+from rest_framework.response import Response
+from .serializers import (
+    QuestionSerializer, OptionSerializer, QuizResultSerializer
+)
+from rest_framework.decorators import api_view
+from .models import Question, Option, QuizResult
+from time import sleep
+
 
 # Create your views here.
+class QuestionListAV(generics.ListCreateAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+
+class QuestionDetailAV(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+    
+    def retrieve(self, request, *args, **kwargs):
+        print('RESULT', self.get_object().get_right_answers())
+        return super().retrieve(request, *args, **kwargs)
+    
+class QuizResultListAV(generics.ListCreateAPIView):
+    queryset = QuizResult.objects.all()
+    serializer_class = QuizResultSerializer
+    
+
+class QuizResultDetailAV(generics.RetrieveUpdateDestroyAPIView):
+    queryset = QuizResult.objects.all()
+    serializer_class = QuizResultSerializer
+
+    
