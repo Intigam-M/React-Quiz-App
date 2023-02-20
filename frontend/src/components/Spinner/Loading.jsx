4f1e831b008790a81c07cadfd400b8ff7da88236
Intigam Mammadli <intigam.mammadli@gmail.com>
2023-02-20 23:41:25 +0400

    loading jsx updated

--- frontend/src/components/Spinner/Loading.jsx
+++ frontend/src/components/Spinner/Loading.jsx
@@ -3,10 +3,10 @@ import classes from './Loading.module.scss'
 
 function Loading() {
   return (
-    <React.Fragment>
+    <>
         <div className={classes.Backdrop}></div>
         <div className={classes.Spinner}><div></div><div></div><div></div><div></div></div>
-    </React.Fragment>
+    </>
   )
 }
 
